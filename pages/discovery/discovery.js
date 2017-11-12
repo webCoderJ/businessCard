2bdd634367c9c9211bde52292b6746d922697d12
pengjie <pengjie@tigerwit.com>
2017-11-12 16:33:05 +0800

    MOD：完善发现 - 列表循环以及点赞

--- pages/discovery/discovery.js
+++ pages/discovery/discovery.js
@@ -9,10 +9,119 @@ Page({
     inputShowed: false,
     inputVal: "",
 
-    // swiper
+    // swiper,导航条
     tabs: Object.values(util.needsMap),
     showtab: 0,
     card: cards.card[0],
+
+    // navi数据由后台配置(假数据)
+    navi: [
+      {
+        name_en: 'co',
+        name_zh: '合作机会',
+        // 剩余条数
+        remian: 0,
+        needs: [
+          {
+            imgs: [
+              '/images/avatar.jpg',
+              '/images/icon-hot.png',
+              '/images/icon-job.png',
+              '/images/icon-co.png',
+              '/images/icon-provide.png'
+            ],
+            id: 0,
+            name: '向日狗',
+            co: '鹏凌科技',
+            position: 'CEOOO',
+            avatar: '/images/avatar.jpg',
+            hot: 20,
+            upvote: 88,
+            upvoted: 0,
+            title: '本公司开发小程序，牛批得很！',
+            desc: '本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!，本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！'
+          },
+          {
+            imgs: [
+              '/images/avatar.jpg',
+              '/images/icon-hot.png',
+              '/images/icon-job.png',
+              '/images/icon-co.png',
+              '/images/icon-provide.png'
+            ],
+            name: '向日狗',
+            co: '鹏凌科技',
+            position: 'CEOOO',
+            avatar: '/images/avatar.jpg',
+            hot: 20,
+            upvote: 88,
+            upvoted: 0,
+            title: '本公司开发小程序，牛批得很！',
+            desc: '本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!，本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！'
+          }, {
+            imgs: [
+              '/images/avatar.jpg',
+              '/images/icon-hot.png',
+              '/images/icon-job.png',
+              '/images/icon-co.png',
+              '/images/icon-provide.png'
+            ],
+            name: '向日狗',
+            co: '鹏凌科技',
+            position: 'CEOOO',
+            avatar: '/images/avatar.jpg',
+            hot: 20,
+            upvote: 88,
+            upvoted: 0,
+            title: '本公司开发小程序，牛批得很！',
+            desc: '本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!，本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！'
+          }
+        ]
+      },
+      {
+        name_en: 'buy',
+        name_zh: '采购计划',
+        remian: 0,
+        needs: [
+          {
+            imgs: [
+              '/images/icon-focus.png',
+            ],
+            name: '戴二肥',
+            co: '鹏凌科技',
+            position: '首席架构',
+            avatar: '/images/avatar.jpg',
+            hot: 20,
+            upvote: 88,
+            upvoted: 1,
+            title: '需要购买1024核CPU',
+            desc: '本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!，本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！'
+          }
+        ]
+      },
+      {
+        name_en: 'job',
+        name_zh: '人才招聘',
+        // 剩余条数
+        remian: 20,
+        needs: [
+          {
+            imgs: [
+              '/images/icon-focus.png',
+            ],
+            name: '刘总',
+            co: '鹏凌科技',
+            position: '首席销售',
+            avatar: '/images/avatar.jpg',
+            hot: 20,
+            upvote: 88,
+            upvoted: 1,
+            title: '需要招聘20个嫩模',
+            desc: '本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!，本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！想做快点给老子打电话！！！Talk is cheap, show me the code!本公司开发小程序，牛批得很！'
+          }
+        ]
+      }
+    ],
 
     // 需求列表
     coList: [...cards.needs.co, ...cards.needs.co],
@@ -21,7 +130,59 @@ Page({
     windowHeight: 0,
     windowWidth: 0
   },
-  showNeedsSelections: function(){
+  // 加载更多
+  upper: function (e) {
+    console.log(e)
+    let oldNavi = this.data.navi
+    let index = e.currentTarget.dataset.index
+    let type = e.currentTarget.dataset.type
+    oldNavi[index].is_refresh = true
+    console.log(oldNavi)
+    this.setData({
+      navi: oldNavi
+    })
+    // 效果模拟
+    setTimeout(() => {
+      oldNavi[index].is_refresh = false
+      this.setData({
+        navi: oldNavi
+      })
+    }, 1000)
+    // util.http.get('/needs/', {
+    //   type: type
+    // }).then(res => {
+    //   oldNavi[index].needs = res.data
+    // })
+  },
+  lower: function(e){
+    // todo - 跟upper一样
+    console.log(e)
+  },
+  // 点赞
+  upvote: function (e) {
+    console.log(e)
+    let oldNavi = this.data.navi
+    let dataset = e.currentTarget.dataset
+    console.log(dataset)
+    oldNavi[dataset.navIndex].needs[dataset.index].upvoted = 1
+    oldNavi[dataset.navIndex].needs[dataset.index].upvote += 1
+    console.log(oldNavi[dataset.navIndex].needs[dataset.index], oldNavi)
+    this.setData({
+      navi: oldNavi
+    })
+
+    // util.http.post('needs/upvote', {
+    //   type: '1',
+    //   id: 'id'
+    // })
+  },
+  // 取消点赞
+  disUpvote: function (e) {
+    util.http.post('needs/upvote', {
+      type: '0'
+    })
+  },
+  showNeedsSelections: function () {
     wx.showActionSheet({
       itemList: Object.values(util.needsMap),
       success: function (res) {
@@ -61,6 +222,11 @@ Page({
       addAnimation: true
     })
 
+    // 获取导航条数据
+    // this.http.get('discovery/navi').then(function(){
+
+    // })
+
     console.log(util.needsMap)
     var that = this;
     wx.getSystemInfo({
@@ -81,13 +247,13 @@ Page({
   },
 
   // swiper
-  togglepage: function (e){
+  togglepage: function (e) {
     console.log(e)
     this.setData({
       showtab: e.detail.current
     })
   },
-  setTab: function (e){
+  setTab: function (e) {
     console.log(e.currentTarget.dataset.tabindex)
     this.setData({
       showtab: e.currentTarget.dataset.tabindex
@@ -98,19 +264,6 @@ Page({
     wx.previewImage({
       current: e.currentTarget.id,
       urls: this.data.card.imgs
-    })
-  },
-  // 点赞
-  upvote: function(e){
-    this.setData({})
-    util.http.post('needs/upvote', {
-      type: '1'
-    })
-  },
-  // 取消点赞
-  disUpvote: function (e) {
-    util.http.post('needs/upvote', {
-      type: '0'
     })
   }
 });
